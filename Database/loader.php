e96ace9a08d72df0052996d9a1dcd8d237eb1e36
Jan-Sebastian Fabik <honza@fabik.org>
2012-06-07 19:24:09 +0200

    Added loader.php.

---
+++ Database/loader.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Database layer for Nette Framework based on Nette\Database.
+ *
+ * @author     Jan-Sebastian Fabík
+ */
+
+require __DIR__ . '/exceptions.php';
+require __DIR__ . '/IModelManager.php';
+require __DIR__ . '/IModelManagerAccessor.php';
+require __DIR__ . '/IRowFactory.php';
+require __DIR__ . '/ActiveRow.php';
+require __DIR__ . '/GroupedSelection.php';
+require __DIR__ . '/ModelManager.php';
+require __DIR__ . '/RowFactory.php';
+require __DIR__ . '/Selection.php';
+require __DIR__ . '/Table.php';
