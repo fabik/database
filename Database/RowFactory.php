2b9728b2cbdfbed1ee7f4d03d3cfb82ba515fb33
Jan-Sebastian Fabik <honza@fabik.org>
2012-06-07 18:32:19 +0200

    Added implementation of RowFactory.

---
+++ Database/RowFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database;
+use Nette\Object;
+
+
+
+/**
+ * Factory for rows.
+ *
+ * @author     Jan-Sebastian Fabík
+ */
+class RowFactory extends Object implements IRowFactory
+{
+	/** @var string[] */
+	protected $classes;
+
+	/** @var string */
+	protected $defaultClass;
+
+
+
+	/**
+	 * @param  string[]
+	 * @param  string
+	 */
+	public function __construct($classes = array(), $defaultClass = 'Database\ActiveRow')
+	{
+		$this->classes = $classes;
+		$this->defaultClass = $defaultClass;
+	}
+
+
+
+	/**
+	 * Creates a new row.
+	 * @param  mixed[]
+	 * @param  Nette\Database\Table\Selection
+	 * @return Nette\Database\Table\ActiveRow
+	 */
+	public function createRow(array $data, \Nette\Database\Table\Selection $table)
+	{
+		$class = $this->getRowClass($table->getName());
+		return new $class($data, $table);
+	}
+
+
+
+	/**
+	 * Gets name of row class for the given table.
+	 * @param  string
+	 * @return string
+	 */
+	public function getRowClass($table)
+	{
+		return isset($this->classes[$table]) ? $this->classes[$table] : $this->defaultClass;
+	}
+}
