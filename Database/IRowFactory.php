6b6d0ed36054d61872734222a55a1e2cefcf0cf8
Jan-Sebastian Fabik <honza@fabik.org>
2012-06-07 17:58:01 +0200

    Added interface IRowFactory.

---
+++ Database/IRowFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database;
+
+
+
+/**
+ * Factory for rows.
+ *
+ * @author     Jan-Sebastian Fabík
+ */
+interface IRowFactory
+{
+	/**
+	 * Creates a new row.
+	 * @param  mixed[]
+	 * @param  Nette\Database\Table\Selection
+	 * @return Nette\Database\Table\ActiveRow
+	 */
+	function createRow(array $data, \Nette\Database\Table\Selection $table);
+}
